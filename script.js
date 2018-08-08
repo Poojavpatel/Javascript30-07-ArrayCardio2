42a870dd852348388573a85856d44cecb7f85e0b
Pooja patel <poojavpatel71@gmail.com>
2018-08-08 21:49:24 +0530

    findindex splice and slice done

--- script.js
+++ script.js
@@ -32,5 +32,33 @@ console.log({adult});
 // every checks if all elements are what u are looking for
 // is everyone 19
 const alladult = people.every( (person) => {
-    
+    const currentyear = (new Date()).getFullYear();
+    return (currentyear -  person.year >= 19);
 });
+console.log(alladult);
+console.log({alladult});
+
+// Find is like filter but instead of returning subset it returns first item it finds
+// find comment with id 823423
+const commenta = comments.find( (comment) => {
+    return (comment.id === 823423);
+});
+console.log(commenta);
+
+//Find Index outputs the index
+const index=comments.findIndex( (comment)=>{
+    return (comment.id === 823423);
+});
+console.log(index);
+
+// How to delete it from the array  2 methods
+// 1)Splice(startfromindex , splicehowmany);  
+comments.splice(index ,1); 
+console.table(comments);
+// 2)make a new array with start 0 to index + (index+1) to the end
+// The ... are the spread operator ,to spead these elements in array
+const newcomments = [
+    ...comments.slice(0,index),
+    ...comments.slice(index+1)
+];
+console.table(newcomments);
